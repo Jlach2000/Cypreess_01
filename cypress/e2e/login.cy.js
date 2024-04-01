23dffae95f4d600b31e33f238675397c32bf87fb
Jlach2000 <joanna.lach@vp.pl>
2024-04-01 23:06:16 +0200

    123

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,13 +1,18 @@
 import { Login } from '../pages/Login'
-import { HomePage } from '../pages/HomePage'
+import { HomePage } from '../pages/Homepage';
+
 const loginPage = new Login();
 const homePage = new HomePage();
+
 describe('Testing the login page', () => {
    it('Test 1: should login and logout with user888@gmail.com', () => {
     loginPage.visit();
     loginPage.login('user888@gmail.com', '1234567890');
-   homePage.logout();});
-});describe('Testing the login page', () => {
+   homePage.logout();
+});
+
+});
+describe('Testing the login page', () => {
     it('Test 1: should login and logout with testowyqa@qa.team', () => {
      loginPage.visit();
      loginPage.login('testowyqa@qa.team', 'QA!automation-1');
--- cypress/pages/Login.js
+++ cypress/pages/Login.js
@@ -1,8 +1,11 @@
 export class Login {
     visit() {
         cy.visit('https://www.edu.goit.global/account/login')
-    }    login(email, password) {
+
+    }  
+      login(email, password) {
         cy.get('#user_email').type(email);
         cy.get('#user_password').type(password);
-        cy.contains('Log in').click();    }
+        cy.contains('Log in').click();  
+      }
 }
